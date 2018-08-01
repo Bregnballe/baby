7f7bd6739be84fe79aa07c2ee718e18693e7fc10
Bregnballe <bregnballe@gmail.com>
2018-08-01 11:03:27 +0200

    Comments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import data from './data/data'
+import data from './data/data' //Making sure our data is available
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App data={data}/>,
+ReactDOM.render(<App data={data}/>, //passing the data into the App
   document.getElementById('root'));
 registerServiceWorker();
